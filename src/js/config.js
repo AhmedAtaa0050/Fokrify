e629d4c25d4e2bfd729fb91ae540181cbd8234e5
AhmedAtaa0050 <ahmedanwar0050011@gmail.com>
2024-04-20 17:23:17 +0200

    init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '12bbdfdc-6442-41aa-8ef7-8a9c4b75ca71';
+export const MODAL_CLOSE_SEC = 2.5;
